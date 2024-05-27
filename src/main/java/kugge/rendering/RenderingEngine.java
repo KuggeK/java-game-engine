8b38e94ad545fc86514802b8f8a1fc2b1cb67d76
Kilian <kilian.kugge@tuni.fi>
2024-05-27 15:09:54 +0300

    Add command-line arguments for choosing renderer

--- src/main/java/kugge/rendering/RenderingEngine.java
+++ src/main/java/kugge/rendering/RenderingEngine.java
@@ -2,24 +2,30 @@ package kugge.rendering;
 
 import kugge.rendering.graphics.Renderer;
 import kugge.rendering.graphics.Window;
-import kugge.rendering.graphics.WindowSettings;
 import kugge.rendering.graphics.opengl.OpenGLRenderer;
 import kugge.rendering.graphics.opengl.OpenGLWindow;
 
 public class RenderingEngine {
-    
     Renderer renderer;
     Window window;
 
     public static void main(String[] args) {
         RenderingEngine engine = new RenderingEngine();
-        engine.start();
+        String type = args.length > 0 ? args[0] : "OPENGL";
+        engine.start(RendererType.valueOf(type));
     }
 
-    public void start() {
-        WindowSettings settings = new WindowSettings(800, 600, "Rendering Engine");
-        window = new OpenGLWindow(settings);
-        renderer = new OpenGLRenderer();
+    enum RendererType {
+        OPENGL
+    }
+
+    public void start(RendererType type) {
+        switch (type) {
+            case OPENGL:
+                renderer = new OpenGLRenderer();
+                window = new OpenGLWindow();
+                break;
+        }
         window.create(renderer);
     }
 }
