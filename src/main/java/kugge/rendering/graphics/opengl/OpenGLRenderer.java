2c04dd413514806ca8440719580ef15e36aa5732
Kilian <kilian.kugge@tuni.fi>
2024-05-31 11:50:57 +0300

    Update OpenGL implementations of windowing and renderer interfaces.

--- src/main/java/kugge/rendering/graphics/opengl/OpenGLRenderer.java
+++ src/main/java/kugge/rendering/graphics/opengl/OpenGLRenderer.java
@@ -1,81 +1,60 @@
 package kugge.rendering.graphics.opengl;
 
-import com.jogamp.opengl.GL4;
-import static com.jogamp.opengl.GL4.*;
 import com.jogamp.opengl.GLAutoDrawable;
-import com.jogamp.opengl.GLEventListener;
-
+import kugge.rendering.core.objects.RenderScene;
 import kugge.rendering.graphics.Renderer;
+import kugge.rendering.graphics.Window;
+public class OpenGLRenderer implements Renderer {
 
-public class OpenGLRenderer implements Renderer, GLEventListener {
-
+    private RenderScene scene;
     private GLAutoDrawable drawable;
+    OpenGLBindings bindings;
+
     private Thread renderThread;
     private final Runnable renderRun = () -> {
         drawable.display();
     };
 
-    public OpenGLRenderer() {
-        renderThread = new Thread(renderRun);
+    public OpenGLRenderer(RenderScene scene) {
+        this.scene = scene;
+        bindings = new OpenGLBindings(scene.getMeshes());
+        renderThread = new Thread(renderRun); 
     }
 
-    /**
-     * Sets the drawable to render to.
-     * @param drawable The drawable to render to.
-     */
-    public void setDrawable(GLAutoDrawable drawable) {
-        this.drawable = drawable;
-        drawable.addGLEventListener(this);
+    public void linkToWindow(Window window) {
+        drawable = ((OpenGLWindow) window).getCanvas();
+        drawable.addGLEventListener(bindings);
     }
 
-    /**
-     * Renders the scene. If the previous render is still running, do nothing.
-     */
     @Override
     public void render() {
         render(false);
     }
 
-    /**
-     * Renders the scene. If force is true and the previous render is still running, 
-     * it will interrupt the previous render and start a new one. If force is false,
-     * it will only render if the previous render is done.
-     * @param force Whether to force the render or not.
-     */
+    @Override
     public void render(boolean force) {
-        if (renderThread.isAlive()) {
-            if (force) {
-                renderThread.interrupt();
-            } else {
-                return;
-            }
+        if (renderThread != null && renderThread.isAlive() && !force) {
+            return;
+        }
+
+        // Set camera information
+        bindings.setFov(scene.getCamera().getFov());
+        bindings.setNear(scene.getCamera().getNear());
+        bindings.setFar(scene.getCamera().getFar());
+        bindings.setViewMatrix(scene.getCamera().getViewMatrix());
+
+        // Set instance information
+        bindings.setInstances(scene.getInstances());
+
+        if (renderThread != null && renderThread.isAlive()) {
+            renderThread.interrupt();
         }
         renderThread = new Thread(renderRun);
         renderThread.start();
     }
 
     @Override
-    public void init(GLAutoDrawable drawable) {
-        GL4 gl = drawable.getGL().getGL4();
-        gl.glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
-        gl.glClear(GL_COLOR_BUFFER_BIT);
+    public void setRenderScene(RenderScene scene) {
+        this.scene = scene;
     }
-
-    @Override
-    public void display(GLAutoDrawable drawable) {
-
-    }
-
-    @Override
-    public void dispose(GLAutoDrawable drawable) {
-        drawable.removeGLEventListener(this);
-        if (renderThread.isAlive()) {
-            renderThread.interrupt();
-        }
-    }
-
-    @Override
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-
-    }    
 }
--- src/main/java/kugge/rendering/graphics/opengl/OpenGLWindow.java
+++ src/main/java/kugge/rendering/graphics/opengl/OpenGLWindow.java
@@ -4,15 +4,14 @@ import javax.swing.JFrame;
 
 import com.jogamp.opengl.awt.GLCanvas;
 
-import kugge.rendering.graphics.Renderer;
 import kugge.rendering.graphics.Window;
 import kugge.rendering.graphics.WindowSettings;
 
 public class OpenGLWindow extends JFrame implements Window {
 
-    private OpenGLRenderer renderer;
     private WindowSettings settings;
     private static final WindowSettings DEFAULT_SETTINGS = new WindowSettings(800, 600, "Window");
+    private GLCanvas canvas;
 
     public OpenGLWindow() {
         this(DEFAULT_SETTINGS);
@@ -23,32 +22,18 @@ public class OpenGLWindow extends JFrame implements Window {
         this.settings = settings;
         updateSettings();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    @Override
-    public void create(Renderer renderer, WindowSettings settings) {
-        this.settings = settings;
-        updateSettings();
-        create(renderer);
-    }
-
-    @Override
-    public void create(Renderer renderer) {
-        if (renderer instanceof OpenGLRenderer) {
-            this.renderer = (OpenGLRenderer) renderer;
-            GLCanvas canvas = new GLCanvas();
-            this.renderer.setDrawable(canvas);
-            add(canvas);
-            setVisible(true);
-        } else {
-            throw new IllegalArgumentException("Renderer must be of type OpenGLRenderer");
-        }
+        canvas = new GLCanvas();
+        add(canvas);
     }
 
     @Override
     public void setWindowSettings(WindowSettings settings) {
         this.settings = settings;
         updateSettings();
+    }
+
+    public GLCanvas getCanvas() {
+        return canvas;
     }
 
     private void updateSettings() {
