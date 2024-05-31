812796fd688937d59a40a144b7c473068266d103
Kilian <kilian.kugge@tuni.fi>
2024-05-31 11:53:05 +0300

    Update main to create and render a scene of a few cubes

--- src/main/java/kugge/rendering/RenderingEngine.java
+++ src/main/java/kugge/rendering/RenderingEngine.java
@@ -1,5 +1,13 @@
 package kugge.rendering;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kugge.rendering.core.objects.Camera;
+import kugge.rendering.core.objects.Cube;
+import kugge.rendering.core.objects.Instance;
+import kugge.rendering.core.objects.Mesh;
+import kugge.rendering.core.objects.RenderScene;
 import kugge.rendering.graphics.Renderer;
 import kugge.rendering.graphics.Window;
 import kugge.rendering.graphics.opengl.OpenGLRenderer;
@@ -8,6 +16,7 @@ import kugge.rendering.graphics.opengl.OpenGLWindow;
 public class RenderingEngine {
     Renderer renderer;
     Window window;
+    RenderScene scene;
 
     public static void main(String[] args) {
         RenderingEngine engine = new RenderingEngine();
@@ -20,12 +29,27 @@ public class RenderingEngine {
     }
 
     public void start(RendererType type) {
+        List<Mesh> meshes = List.of(
+            Cube.withSize(1, 0.1f),
+            Cube.withSize(2, 0.3f)
+        );
+        List<Instance> instances = new ArrayList<>();
+
+        for (int i = -5; i < 5; i++) {
+            instances.add(new Instance(1, new float[] {i, 0, 0}, new float[] {0, 0, 0}, new float[] {1, 1, 1}, null));
+            instances.add(new Instance(2, new float[] {0, i, 0}, new float[] {0, 0, 0}, new float[] {1, 1, 1}, null));
+        }
+
+        scene = new RenderScene(new Camera(), meshes, instances);
+
         switch (type) {
             case OPENGL:
-                renderer = new OpenGLRenderer();
+                renderer = new OpenGLRenderer(scene);
                 window = new OpenGLWindow();
-                break;
+                break;        
         }
-        window.create(renderer);
+
+        renderer.linkToWindow(window);
+        renderer.render();
     }
 }
---
+++ src/main/java/kugge/rendering/core/objects/Cube.java
@@ -0,0 +1,53 @@
+package kugge.rendering.core.objects;
+
+import kugge.rendering.core.objects.materials.Material;
+
+public class Cube extends Mesh {
+
+    public Cube(int id, float[] positions, float[] textureCoords, float[] normals, int[] indices) {
+        super(id, positions, textureCoords, normals, indices);
+    }
+
+    public Cube(int id, float[] positions, float[] textureCoords, float[] normals, int[] indices, Material material) {
+        super(id, positions, textureCoords, normals, indices, material);
+    }
+
+    public static Cube withSize(int id, float size) {
+        float[] positions = new float[] {
+            -size, -size, -size,
+            -size, size, -size,
+            size, -size, -size,
+            size, size, -size,
+            -size, -size, size,
+            -size, size, size,
+            size, -size, size,
+            size, size, size
+        };
+        float[] textureCoords = new float[] {
+            0, 0,
+            0, 1,
+            1, 0,
+            1, 1,
+            0, 0,
+            0, 1,
+            1, 0,
+            1, 1
+        };
+        float[] normals = positions;
+        int[] indices = new int[] {
+            0, 1, 2,
+            1, 3, 2,
+            4, 6, 5,
+            5, 6, 7,
+            0, 4, 1,
+            1, 4, 5,
+            2, 3, 6,
+            3, 7, 6,
+            0, 2, 4,
+            2, 6, 4,
+            1, 5, 3,
+            3, 5, 7
+        };
+        return new Cube(id, positions, textureCoords, normals, indices);
+    }
+}
