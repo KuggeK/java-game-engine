8b7284db86d5b75aad16d3e4ae5e62660974ad2d
Kilian <kilian.kugge@tuni.fi>
2024-08-20 10:06:09 +0300

    Separate different collider types to separate classes

---
+++ src/main/java/io/github/kuggek/engine/ecs/components/physics/colliders/BoxColliderComponent.java
@@ -0,0 +1,39 @@
+package io.github.kuggek.engine.ecs.components.physics.colliders;
+
+import org.joml.Vector3f;
+import org.ode4j.ode.DBox;
+
+import io.github.kuggek.engine.ecs.components.ComponentField;
+import io.github.kuggek.engine.subsystems.EngineRuntimeSettings;
+
+public class BoxColliderComponent extends PhysicsColliderComponent {
+    
+    @ComponentField
+    private Vector3f scale;
+
+    public BoxColliderComponent() {
+        super(ColliderType.BOX);
+        scale = new Vector3f(1);
+    }
+
+    @Override
+    public void setCollider(ColliderType colliderType) {
+        throw new UnsupportedOperationException("Cannot change collider type of BoxColliderComponent");
+    }
+
+    @Override
+    public void setScale(Vector3f scale) {
+        DBox box = (DBox) getCollider();
+        box.setLengths(scale.x, scale.y, scale.z);
+        this.scale.set(scale);
+    }
+
+    public Vector3f getScale() {
+        return scale;
+    }
+
+    @Override
+    protected void onAwake(EngineRuntimeSettings settings) {
+        setScale(scale);
+    }
+}
---
+++ src/main/java/io/github/kuggek/engine/ecs/components/physics/colliders/SphereColliderComponent.java
@@ -0,0 +1,41 @@
+package io.github.kuggek.engine.ecs.components.physics.colliders;
+
+import org.joml.Vector3f;
+import org.ode4j.ode.DSphere;
+
+import io.github.kuggek.engine.ecs.components.ComponentField;
+import io.github.kuggek.engine.subsystems.EngineRuntimeSettings;
+
+public class SphereColliderComponent extends PhysicsColliderComponent {
+
+    @ComponentField
+    private float radius;
+
+    public SphereColliderComponent() {
+        super(ColliderType.SPHERE);
+    }
+
+    @Override
+    public void setCollider(ColliderType colliderType) {
+        throw new UnsupportedOperationException("Cannot change collider type of SphereColliderComponent");
+    }
+
+    public void setRadius(float radius) {
+        ((DSphere) getCollider()).setRadius(radius);
+        this.radius = radius;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    @Override
+    public void setScale(Vector3f scale) {
+        setRadius(scale.x);
+    }
+
+    @Override
+    protected void onAwake(EngineRuntimeSettings settings) {
+        setRadius(radius);
+    }
+}
